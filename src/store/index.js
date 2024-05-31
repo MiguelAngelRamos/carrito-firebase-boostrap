b3787b2f41dd4527f86869b12d3cdb192c541ce1
Miguel Angel Ramos <return.infox@gmail.com>
2024-05-31 10:08:15 -0400

    app de productos esta completamente terminada

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from 'vuex';
 import { firebaseApp } from '../config/firebase';
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, createUserWithEmailAndPassword, signOut } from 'firebase/auth';
 
 const auth = getAuth(firebaseApp); // con esto quiero gestionar el autenticación del usuario
 
@@ -38,6 +38,25 @@ export default createStore({
         commit('setAuthUser', null);
         router.push({name: 'login'})
       }).catch(error => console.error(error))
+    },
+    register({commit}, { email, password, router }) {
+     
+      createUserWithEmailAndPassword(auth, email, password)
+        .then(userCredential => {
+          commit('setAuthUser', userCredential.user);
+          router.push({name: 'home'}) // creas una cuenta y te redirige al home
+        })
+        .catch( error => {
+          console.error(error);
+          commit('setErrorMsg', 'Error al crear la cuenta')
+        })
+    },
+    initAuth({ commit }) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          commit('setAuthUser', user);
+        }
+      });
     }
   }, 
   getters: {
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,16 +7,16 @@ import { computed } from 'vue';
 
 // importar Vuex
 const store = useStore();
-const { handleSubmit }= useForm({ validationSchema });
+const { handleSubmit } = useForm({ validationSchema });
 
 const router = useRouter();
 const email = useField('email');
 const password = useField('password');
 
 
-const submit = handleSubmit( values => {
+const submit = handleSubmit(values => {
 
-  store.dispatch('login',{...values, router})
+  store.dispatch('login', { ...values, router })
 })
 
 const hasError = computed(() => store.state.errorMsg)
@@ -31,35 +31,32 @@ const hasError = computed(() => store.state.errorMsg)
         <form class="p-4 shadow">
 
           <div v-if="hasError" class="alert alert-danger" role="alert">
-             {{ store.state.errorMsg }}
+            {{ store.state.errorMsg }}
           </div>
 
           <div class="mb-3">
             <label for="email" class="form-label">Correo Eletronico: </label>
-            <input 
-              type="email" 
-              class="form-control" 
-              id="email" 
-              v-model="email.value.value"
-              :class="{'is-invalid': email.errorMessage.value ? true: false }"
-              required>
-            <div v-if="email.errorMessage" id="emailHelp" class="text-danger form-text">{{ email.errorMessage.value }}</div>
+            <input type="email" class="form-control" id="email" v-model="email.value.value"
+              :class="{ 'is-invalid': email.errorMessage.value ? true : false }" required>
+            <div v-if="email.errorMessage" id="emailHelp" class="text-danger form-text">{{ email.errorMessage.value }}
+            </div>
           </div>
 
           <div class="mb-3">
             <label for="password" class="form-label">Contraseña: </label>
-            <input 
-              type="password" 
-              class="form-control" 
-              id="password" 
-              v-model="password.value.value"
-              :class="{'is-invalid': password.errorMessage.value ? true: false }"
-              required>
-            <div v-if="password.errorMessage" id="emailHelp" class="text-danger form-text">{{ password.errorMessage.value }}</div>
+            <input type="password" class="form-control" id="password" v-model="password.value.value"
+              :class="{ 'is-invalid': password.errorMessage.value ? true : false }" required>
+            <div v-if="password.errorMessage" id="emailHelp" class="text-danger form-text">{{
+              password.errorMessage.value }}</div>
           </div>
 
           <button type="submit" class="btn btn-dark w-100" @click="submit">Ingresar</button>
+          <div class="text-center mt-3">
+            ¿No tienes cuenta? <router-link to="/register">Regístrate</router-link>
+          </div>
         </form>
+
+
 
       </div>
     </div>
